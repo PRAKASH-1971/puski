d37b25ab94ca539ddd81bd8739f5f6a4f2247ada
PRAKASH-1971 <84118928+PRAKASH-1971@users.noreply.github.com>
2021-05-18 15:30:38 +0530

    Add files via upload

---
+++ my component/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { Footer } from "./my component/Footer";
+
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus recusandae 
+                itaque quo necessitatibus nostrum nesciunt 
+                illum ipsum? Earum exercitationem ipsam nam placeat
+                 itaque omnis neque, incidunt iusto doloribus aperiam 
+                 quia ipsum eum, impedit iste?</p>
+        </div>
+    )
+}
